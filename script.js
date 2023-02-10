064169462a83b7d0565ddc2ded9bea2b41ef4db3
Matt <matthieuosten.0@gmail.com>
2023-02-10 11:58:12 +0100

    Add SocialNetwork logo and Major Update of display

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 var labels = document.querySelectorAll(".label");
 for (var i = 0; i < labels.length; i++) {
   labels[i].addEventListener("mouseover", function() {
-    this.style.transform = "translateY(-100%)";
+    this.style.transform = "translateY(-20%)";
     this.style.zIndex = "1";
+    this.style.animation = '';
   });
   labels[i].addEventListener("mouseout", function() {
     this.style.transform = "translateY(0)";
     this.style.zIndex = "0";
+    this.style.animation = 'float 1.5s ease-in-out infinite';
+    this.style.animationDelay = (i * 0.5) + "s";
   });
 }
 
@@ -22,24 +25,122 @@ window.onscroll = function() {
   }
 };
 
-document.getElementById("file-input").addEventListener("change", function() {
-  console.log("Input file changed");
-  let file = this.files[0];
-  console.log("File selected: " + file.name);
-  let reader = new FileReader();
-
-  reader.onloadend = function() {
-      console.log("File reading completed");
-      let data = JSON.parse(reader.result);
-      console.log("Data parsed: " + JSON.stringify(data));
-      data.pages.forEach(page => {
-          console.log("Creating li for: " + page.name);
-          let li = document.createElement("li");
-          li.href = page.url;
-          li.innerHTML = page.name;
-          sidebar.appendChild(li);
-      });
+/* Utiliser JavaScript pour détecter le défilement de la page */
+window.onscroll = function() {
+  if (window.pageYOffset > 500) {
+    document.querySelector(".scroll-top-button").style.opacity = "1";
+    document.querySelector(".scroll-top-button").style.visibility = "visible";
+  } else {
+    document.querySelector(".scroll-top-button").style.opacity = "0";
+    document.querySelector(".scroll-top-button").style.visibility = "hidden";
   }
-  reader.readAsText(file);
-  console.log("File reading started");
-}, false);
+
+  var mainContent = document.querySelector('.main-content');
+  var sidebar = document.querySelector('.sidebar');
+
+  sidebar.style.height = mainContent.offsetHeight + 'px';
+};
+
+/* Utiliser JavaScript pour détecter le chargement de la page */
+window.onload = function() {
+  document.querySelector(".loading-screen").style.opacity = "1";
+  document.querySelector(".loading-screen").style.visibility = "visible";
+};
+
+window.addEventListener("load", function() {
+  var labels = document.querySelectorAll(".label");
+  for (var i = 0; i < labels.length; i++) {
+    labels[i].style.animation = "float 1.5s ease-in-out infinite";
+    labels[i].style.animationDelay = (i * 0.5) + "s";
+  }
+
+  /* Utiliser JavaScript pour gerer la neige */
+
+  var snowflakesContainer = document.querySelector('.snowflakes-container');
+  var totalSnowflakes = 200;
+
+  for (let i = 0; i < totalSnowflakes; i++) {
+    var snowflake = document.createElement("div");
+    snowflake.classList.add('snowflake');
+    snowflake.style.left = Math.random() * 100 + 'vw';
+    snowflake.style.animationDelay = Math.random() * 5 + 's';
+    snowflakesContainer.appendChild(snowflake);
+  }
+
+/* Reitre la page de chargement */
+  document.querySelector(".loading-screen").style.opacity = "0";
+  document.querySelector(".loading-screen").style.visibility = "hidden";
+});
+
+
+/* JavaScript pour faire tourner les box vers la souris */
+const boxes = document.querySelectorAll(".box");
+const limits = 20;
+
+// Declare an anonymous function to handle the mousemove event
+boxes.forEach(box => {
+  box.addEventListener("mousemove", function (e) {
+    // Get the bounding rectangles of the target element
+    const rect = e.target.getBoundingClientRect();
+
+    // Calculate the x and y position of the mouse within the element
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    // Calculate the offset of the mouse within the element as a decimal
+    const offsetX = x / rect.width;
+    const offsetY = y / rect.height;
+
+    // Calculate the rotateX and rotateY values based on the mouse offset
+    const rotateY = (offsetX) * (limits * 2) - limits;
+    const rotateX = (offsetY) * (limits * 2) - limits;
+
+    // Calculate the shadow offset values based on the mouse offset
+    const shadowOffsetX = (offsetX) * 32 - 16;
+    const shadowOffsetY = (offsetY) * 32 - 16;
+
+    // Update the element's styles to apply the shadow and rotation
+    this.style.boxShadow = (1 / 6) * -shadowOffsetX + "px " + (1 / 6) * -shadowOffsetY + "px 3px rgba(0, 0, 0, 0.051), " +
+      (2 / 6) * -shadowOffsetX + "px " + (2 / 6) * -shadowOffsetY + "px 7.2px rgba(0, 0, 0, 0.073), " +
+      (3 / 6) * -shadowOffsetX + "px " + (3 / 6) * -shadowOffsetY + "px 13.6px rgba(0, 0, 0, 0.09), " +
+      (4 / 6) * -shadowOffsetX + "px " + (4 / 6) * -shadowOffsetY + "px 24.3px rgba(0, 0, 0, 0.107), " +
+      (5 / 6) * -shadowOffsetX + "px " + (5 / 6) * -shadowOffsetY + "px 45.5px rgba(0, 0, 0, 0.129), " +
+      -shadowOffsetX + "px " + -shadowOffsetY + "px 109px rgba(0, 0, 0, 0.18)";
+
+    this.style.transform = "perspective(1000px) rotateX(" + -rotateX + "deg) rotateY(" + rotateY + "deg)";
+
+     // Calculate the glare position based on the rotation values
+    const glarePos = rotateX + rotateY + 90;
+    const glare = this.querySelector(".glare");
+    glare.style.left = glarePos + "%";
+  });
+
+  box.addEventListener("mouseleave", function (e) {
+    this.style.boxShadow = "0px 0px 3px rgba(0, 0, 0, 0.051), 0px 0px 7.2px rgba(0, 0, 0, 0.073), 0px 0px 13.6px rgba(0, 0, 0, 0.09), 0px 0px 24.3px rgba(0, 0, 0, 0.107), 0px 0px 45.5px rgba(0, 0, 0, 0.129), 0px 0px 109px rgba(0, 0, 0, 0.18)";
+    this.style.transform = "scale(1.0)";
+    const glare = this.querySelector(".glare");
+    glare.style.left = "100%";
+  });
+});
+
+/* Utiliser JavaScript pour mettre chaque charactere du titre dans une balise span */
+var balise = document.querySelector('.char-to-char');
+var text = balise.innerHTML;
+balise.innerHTML = ""; // Supprime le texte précédent
+
+for (var i = 0; i < text.length; i++) {
+  var letter = document.createElement("span");
+  letter.style.animationDelay = (i * 0.1) + "s";
+  letter.innerHTML = text[i];
+  balise.appendChild(letter);
+}
+
+/* Utiliser JavaScript pour mettre chaque balise du parent */
+var baliseTab = document.querySelector('.balise-to-balise');
+if (baliseTab != null) {
+  for (var i = 0; i < baliseTab.children.length; i++) {
+    baliseTab.children[i].style.animationDelay = (i * 0.5) + "s";
+  }
+}
+
+
